d44618c5fb7f37669139bab6c34089e970ef56c0
RickDev <henriiisilva2007@gmail.com>
2026-08-26 22:43:46 -0300

    De volta aos primórdios

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        String nome = "";
+        String cidade = "";
+        int idade;
+        System.out.println("Digite seu nome: ");
+       nome = sc.nextLine();
+        System.out.println("Perfeito " +nome+ ", Agora digite a sua cidade: ");
+        cidade = sc.nextLine();
+        System.out.println("Muito legal " +nome+ "!! A cidade de " +cidade+ " é realmente Muito legal!");
+        System.out.println("Agora digite a sua idade");
+        idade = sc.nextInt();
+        if (idade >= 0 || idade <= 17) {
+            System.out.println("Ah que bacana! voce ainda esta no auge da juventude");
+        } else if (idade >= 18 || idade <= 59) {
+            System.out.println("Que legal! voce já é um adulto!");
+        } else if (idade >= 60) {
+            System.out.println("Bacana! voce ja viveu muitas histórias para contar!");
+        };
+        System.out.println("foi muito legal conversar com voce, talvez podemos conversar novamente algum dia, pode ser?");
+        System.out.println("Até logo!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
 }
