aad59750ec5e710d442a90321e2af539aaa0eb7c
RickDev <henriiisilva2007@gmail.com>
2026-09-03 17:38:38 -0300

    finalizacao do exercicio

--- src/exercicio02.java
+++ src/exercicio02.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class exercicio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Dev em transiçao registrado: " + nome.toUpperCase());
+        System.out.println("Status: focado no java e no futuro.");
+        sc.close();
+    }
 }
