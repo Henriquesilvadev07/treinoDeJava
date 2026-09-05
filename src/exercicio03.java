1ab0c0bc055f511595f01704a9f3ce5d3533cd80
RickDev <henriiisilva2007@gmail.com>
2026-09-04 22:15:56 -0300

    finalizacao do exercicio

--- src/exercicio03.java
+++ src/exercicio03.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
 
 public class exercicio03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite a senha: ");
+        String senha = sc.nextLine();
+        if (senha.length() >= 8){
+            System.out.println("Senha com tamanho suficiente e seguro");
+        }else {
+            System.out.println("A senha tem menos que 8 digitos, crie outra senha!");
+        }
+        sc.close();
+    }
 }
