fe2132ccf7ca0bacab060c1055a4924d3a1df4eb
RickDev <henriiisilva2007@gmail.com>
2026-09-15 23:11:35 -0300

    Finalizacao do exercicio 05

--- src/exercicio05.java
+++ src/exercicio05.java
@@ -2,5 +2,13 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class exercicio05 {
+    public static void main(String[] args) {
+        LocalDate hoje = LocalDate.now();
+        LocalDate fimDoAno = LocalDate.of(hoje.getYear(), 12, 31);
+
+        Long diasRestantes = ChronoUnit.DAYS.between(hoje, fimDoAno);
+
+        System.out.println("Faltam " +diasRestantes+ " Dias para o fim do ano");
+    }
 
 }
